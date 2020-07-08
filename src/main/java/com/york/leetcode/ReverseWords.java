389c551e29434a997402e7a7cfb42570398cf178
york <377262688@qq.com>
2020-07-08 09:30:40 +0800

    增加字符串反转

---
+++ src/main/java/com/york/leetcode/ReverseWords.java
@@ -0,0 +1,60 @@
+package com.york.leetcode;
+
+import java.util.*;
+
+/**
+ * @author york
+ * @create 2020-07-08 09:05
+ **/
+public class ReverseWords {
+
+    public static String reverseWords(String s) {
+        s = s.trim();
+        System.out.println(s);
+        List<String> wordList = Arrays.asList(s.split("\\s+"));
+        System.out.println(wordList);
+        Collections.reverse(wordList);
+        return String.join(" ",wordList);
+    }
+
+    public static void main(String[] args) {
+        String s = "   hello world!";
+        System.out.println(reverseWords1(s));
+        String s1 = " a good   example";
+        System.out.println(reverseWords1(s1));
+    }
+
+    public static String reverseWords1(String s) {
+        int left = 0;
+        int right = s.length() - 1;
+        while (left <= right && s.charAt(left) == ' ') {
+            left++;
+        }
+        while (right >= left && s.charAt(right) == ' ') {
+            right--;
+        }
+        StringBuilder sb = new StringBuilder();
+        Stack<String> stack = new Stack<>();
+        while (left <= right) {
+            char c = s.charAt(left);
+            if (c == ' ' && sb.length() != 0) {
+                stack.push(sb.toString());
+                sb.setLength(0);
+            } else if (c != ' ') {
+                sb.append(c);
+            }
+            left++;
+        }
+        if (sb.length() > 0) {
+            stack.push(sb.toString());
+        }
+        String reslut = "";
+        while (!stack.isEmpty()) {
+            reslut = reslut + stack.pop();
+            if (!stack.isEmpty()) {
+                reslut = reslut + " ";
+            }
+        }
+        return reslut;
+    }
+}
