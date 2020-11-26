f3f906f6a74d53c7109c0618f9b0170a7c47f8e2
york <377262688@qq.com>
2020-11-26 22:43:46 +0800

    增加位运算应用

--- src/main/java/com/york/leetcode/BinaryTree.java
+++ src/main/java/com/york/leetcode/BinaryTree.java
@@ -1,12 +1,16 @@
 package com.york.leetcode;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
 /**
  * @author york
  * @create 2020-11-25 17:34
  **/
 public class BinaryTree {
 
-    private int getBinaryTreeDepth(BinaryTreeNode node) {
+    private int getBinaryTreeDepth(TreeNode node) {
         if (node == null) {
             return 0;
         } else {
@@ -16,17 +20,57 @@ public class BinaryTree {
         }
     }
 
-    static class BinaryTreeNode {
-        private Object value;
-
-        private BinaryTreeNode left;
-
-        private BinaryTreeNode right;
-
-        public BinaryTreeNode(Object value, BinaryTreeNode left, BinaryTreeNode right) {
-            this.value = value;
-            this.left = left;
-            this.right = right;
+    private List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+        if (root == null) {
+            return list;
         }
+        Stack<TreeNode> stack = new Stack<>();
+
+
+        list.addAll(inorderTraversal(root.left));
+        list.add(root.val);
+        list.addAll(inorderTraversal(root.right));
+        return list;
+    }
+
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        TreeNode root = null;
+        if (preorder.length != inorder.length) {
+            throw new IllegalArgumentException();
+        }
+        if (preorder.length == 0 || inorder.length == 0) {
+            return root;
+        }
+
+        return root;
+    }
+
+    public static void isTwoMi(int n) {
+        System.out.println((n - 1) & n);
+    }
+
+    public static void numberOf1(int n) {
+        int count = 0;
+        int flag = 1;
+        while (flag > 0) {
+            if ((n & flag) != 0) {
+                count++;
+            }
+            flag = flag << 1;
+        }
+        System.out.println(count);
+    }
+    public static void main(String[] args) {
+        isTwoMi(8);
+        numberOf1(-15);
+    }
+
+    static class TreeNode {
+        private Integer val;
+
+        private TreeNode left;
+
+        private TreeNode right;
     }
 }
